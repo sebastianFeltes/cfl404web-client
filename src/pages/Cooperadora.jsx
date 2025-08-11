52cef38d2585ab24bf1b66d4372cfdf470234080
hernanzacary <hernanzacary@gmail.com>
2025-08-11 20:29:33 -0300

    add cooperadora textos

--- src/pages/Cooperadora.jsx
+++ src/pages/Cooperadora.jsx
@@ -1,6 +1,49 @@
 function Cooperadora() {
   return (
-    <div>Cooperadora</div>
+    <div className="h-full w-full">
+      <h2 className="text-3xl font-poppins border-2 p-2">COOPERADORA CFL 404 BERISSO</h2>
+      <div>
+        <p>
+Nuestra Asociación Cooperadora
+¿Sabías que el Centro de Formación Laboral N.º 404 de Berisso cuenta con una Asociación Cooperadora?
+Esta cooperadora está oficialmente reconocida por la Dirección de Cooperación Escolar de la Provincia de Buenos Aires y trabaja día a día para fortalecer nuestra institución.
+
+Gracias a los aportes y colaboraciones recibidas, podemos realizar mejoras y cubrir necesidades esenciales, destinando casi la totalidad de los fondos al mantenimiento y cuidado del edificio, como así también a la compra de insumos para las prácticas de los alumnos.
+Cada contribución es una inversión directa en un espacio de aprendizaje seguro, cómodo y de calidad para todos.
+
+Tu ayuda hace la diferencia.
+Un pequeño gesto… un gran cambio para todos
+Invertí en tu aprendizaje. Apoyá a tu cooperadora
+
+¿Queres colaborar?
+Podes hacerlo con la siguiente información:
+
+Banco Provincia de Buenos Aires
+ALIAS: coop.cfl.404
+CBU: 0140032801503305438550
+CUIT: 30-71753985-7
+
+IMPORTANTE: Por favor, luego de realizar la transferencia enviar el comprobante mediante WhatsApp al 221-319-2360.
+Muchas gracias!!!
+        </p>
+      </div>
+    
+    
+    
+    {/* Se armó maquetado DIV para agregar Memorias */}
+    <div className="h-96 w-full border-4">ESPACIO RESERVADO PARA MEMORIAS
+      <p>
+      Memorias de Nuestro Camino
+A lo largo de los años, el Centro de Formación Laboral N.º 404 de Berisso ha sido escenario de innumerables proyectos, logros y momentos compartidos.
+Este espacio es una ventana al pasado, donde las imágenes cuentan la historia de los emprendimientos, talleres y actividades que dejaron huella en nuestra comunidad educativa.
+
+Cada fotografía es más que un recuerdo: es testimonio del esfuerzo, la creatividad y la pasión de quienes formaron y forman parte de esta institución.
+
+Revivamos juntos estas memorias que nos inspiran a seguir creciendo.
+      </p>
+    </div>
+    
+    </div>
   )
 }
 
