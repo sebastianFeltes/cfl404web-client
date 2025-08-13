1d08ba482e6205f5bd52fd3148f4b76647815e20
lucho-22 <lupemonautico@gmail.com>
2025-08-13 19:34:10 -0300

    add estadisticas hover

--- src/pages/Estadisticas.jsx
+++ src/pages/Estadisticas.jsx
@@ -1,8 +1,54 @@
 function Estadisticas() {
   return (
     <div className="h-full w-full">
-      
-      
+      <h1 className="px-4 py-4 font-poppins text-3xl bg-customDark-blue text-white">
+        Estadisticas
+      </h1>
+      <div className="flex flex-row gap-4 justify-center border  rounded-lg px-2 py-2 font-poppins">
+        <ul>
+          <li>
+            <button className="relative group px-6 py-2 text-lg font-semibold text-black rounded-md overflow-hidden">
+              Asistencia
+              <span className="absolute left-0 bottom-0 h-[3px] w-0 bg-customDark-blue transition-all duration-300 group-hover:w-full"></span>
+            </button>
+            {/* <a href="">Asistencias</a> */}
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <button className="relative group px-6 py-2 text-lg font-semibold text-black rounded-md overflow-hidden">
+              Egreso de Alumnos
+              <span className="absolute left-0 bottom-0 h-[3px] w-0 bg-customDark-blue transition-all duration-300 group-hover:w-full"></span>
+            </button>
+            {/* <a href="">Egreso de Alumnos</a> */}
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <button className="relative group px-6 py-2 text-lg font-semibold text-black rounded-md overflow-hidden">
+              Datos Historicos
+              <span className="absolute left-0 bottom-0 h-[3px] w-0 bg-customDark-blue transition-all duration-300 group-hover:w-full"></span>
+            </button>
+            {/* <a href="">Datos Historicos</a> */}
+          </li>
+        </ul>
+        <ul>
+          <li>
+             <button className="relative group px-6 py-2 text-lg font-semibold text-black rounded-md overflow-hidden">
+              Graficos
+              <span className="absolute left-0 bottom-0 h-[3px] w-0 bg-customDark-blue transition-all duration-300 group-hover:w-full"></span>
+            </button>
+            {/* <a href="">Graficos</a> */}
+          </li>
+        </ul>
+      </div>
+    
+    
+    
+    
+    
+    
+    
     </div>
   );
 }
--- src/pages/Institucional.jsx
+++ src/pages/Institucional.jsx
@@ -1,16 +1,6 @@
 function Institucional() {
   return (
-    <div className="h-full w-full">
-<h1 className="font-poppins  text-3xl bg-blue-800 text-white px-4 py-2">institucional</h1>
-      <span className="items-center justify-center gap-6 font-poppins text-blue-800">Ubicacion, infraestructura y tecnologia</span>
-      <span className="items-center justify-center gap-6 font-poppins text-blue-800">Programa de beneficios para alumnos</span>
-      <span className="items-center justify-center gap-6 font-poppins text-blue-800">Estadisticas educacionales y economicas</span>
-
-
-
-
-
-    </div>
+    <div>Institucional</div>
   )
 }
 
