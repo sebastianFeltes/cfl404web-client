35c7045106bede7667a3acd43350a2d7fd51f0da
hernanzacary <hernanzacary@gmail.com>
2025-08-22 17:45:57 -0300

    fix colores de marcos, tipografia y centrados

--- src/pages/Cooperadora.jsx
+++ src/pages/Cooperadora.jsx
@@ -1,13 +1,13 @@
 function Cooperadora() {
   return (
-    <div className="h-full w-full flex flex-col justify-evenly gap-8 pb-8">
-     {/* <img src="https://images.pexels.com/photos/3184397/pexels-photo-3184397.jpeg" alt="imagen de fondo">*/}
+    <div className="h-full w-full flex flex-col justify-evenly gap-8 pb-8 bg-[url(´https://images.pexels.com/photos/3184397/pexels-photo-3184397.jpeg´)] bg-cover bg-center">
+      {/* <img src="https://images.pexels.com/photos/3184397/pexels-photo-3184397.jpeg" alt="imagen de fondo">*/}
       <h2 className="text-3xl text-white font-poppins font-medium p-2 text-center bg-customDark-blue">
         COOPERADORA CFL 404 BERISSO
       </h2>
 
-      <div className="flex h-1/3 px-8 bg-white">
-        <p className="font-poppins  shadow-lg shadow-customSoft-blue p-2 rounded-lg">
+      <div className="flex h-1/3 px-8">
+        <p className="font-poppins  shadow-lg shadow-customGray p-2 rounded-lg">
           Nuestra Asociación Cooperadora ¿Sabías que el Centro de Formación
           Laboral N.º 404 de Berisso cuenta con una Asociación Cooperadora? Esta
           cooperadora está oficialmente reconocida por la Dirección de
@@ -25,18 +25,22 @@ function Cooperadora() {
       </div>
 
       <div className="px-8 h-1/2 flex justify-between gap-8">
-        <div className="border shadow-lg shadow-customSoft-blue p-2 rounded-lg w-1/2 bg-white">
+        <div className="border shadow-lg  shadow-customGray p-2 rounded-lg w-1/2 bg-white text-center">
           <p className="font-poppins">
-            ¿Queres colaborar? Podes hacerlo con la siguiente información: Banco
-            Provincia de Buenos Aires ALIAS: coop.cfl.404 CBU:
-            0140032801503305438550 CUIT: 30-71753985-7 IMPORTANTE: Por favor,
-            luego de realizar la transferencia enviar el comprobante mediante
-            WhatsApp al 221-319-2360. Muchas gracias!!!
+            ¿Queres colaborar? Podes hacerlo con la siguiente información:
+          </p>
+          <p className="font-serif font-bold">Banco Provincia de Buenos Aires</p>
+          <p className="font-serif font-bold">ALIAS: coop.cfl.404</p>
+          <p className="font-serif font-bold">CBU: 0140032801503305438550</p>
+          <p className="font-serif font-bold">CUIT: 30-71753985-7</p>
+          <p className="font-poppins">
+            IMPORTANTE: Por favor, luego de realizar la transferencia enviar el
+            comprobante mediante WhatsApp al 221-319-2360. Muchas gracias!!!
           </p>
         </div>
 
         {/* Se armó maquetado DIV para agregar Memorias */}
-        <div className="border shadow-lg shadow-customSoft-blue p-2 rounded-lg  w-1/2 bg-white">
+        <div className="border shadow-lg  shadow-customGray p-2 rounded-lg  w-1/2 bg-white">
           ESPACIO RESERVADO PARA MEMORIAS
           <p className="font-poppins">
             Memorias de Nuestro Camino A lo largo de los años, el Centro de
